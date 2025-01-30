96b66083810c3dc76567cff3a72b01181cfb0362
johan-tvarghese <johan.thomasvarghese8@mail.dcu.ie>
2025-01-30 10:39:23 +0000

    Update path_finding.cpp

--- path_finding.cpp
+++ path_finding.cpp
@@ -1,46 +1,123 @@
-
-using namespace std;
 #include <vector>
-#include <iostream>
+
+int CurrentNode = 1;
+int Destination = 2;
+std::vector<std::vector<int>> matrix = {{ 0, 0, 0, 0, 1, 1, 0 },
+                              { 0, 0, 0, 0, 0, 1, 1 },
+                              { 0, 0, 0, 1, 0, 1, 0 },
+                              { 0, 0, 1, 0, 0, 0, 1 },
+                              { 1, 0, 0, 0, 0, 0, 1 },
+                              { 1, 1, 1, 0, 0, 0, 0 },
+                              { 0, 1, 0, 1, 1, 0, 0 }};
 
 
-int path(const vector<vector<int>>& matrix, int CurrentNode, int Destination){
-    if (matrix[CurrentNode][Destination] > 0){
-        cout << "path is" << CurrentNode << " -> " << Destination << endl;
-        return 1;
-    }
-    else if(matrix[CurrentNode][Destination] < 0){
-        cout << "path is" << CurrentNode << " -> " << Destination << endl;
-        return -1;
+void setup(){
+  Serial.begin(9600);
+
+}
+
+std::vector<int> path(const std::vector<std::vector<int>>& matrix, int CurrentNode, int Destination){
+    int Node1;
+    std::vector<int> route;
+    if (matrix[CurrentNode][Destination] != 0){
+        route = {CurrentNode,Destination};
+        return route;
     }
     else{
-        for(int j=0; j<7;j++){
-           cout << "For loop 1" << endl;				//debugging line
-           if (matrix[CurrentNode][j] != 0){
-                cout << "If loop 1"<< endl;			//debugging line
-                if (matrix[j][Destination] > 0){
-                    cout << "If loop 2" <<endl;			//debugging line
-                    cout << "Path is " << CurrentNode << " -> " << j << " -> "<<Destination << endl;
-                    return 1;
-                }
-                else if(matrix[j][Destination] < 0){
-                    cout << "else If loop 1" <<endl;			//debugging line
-                    cout << "Path is " << CurrentNode << " -> " << j << " -> "<<Destination << endl;
-                    return -1;
-
+        if(CurrentNode == 4 || CurrentNode == 3){
+           for(int j=0; j<7;j++){
+              if (matrix[CurrentNode][j] != 0){
+                if (matrix[j][Destination] != 0){
+                    if (j != 5 && j != 6){
+                        route = {CurrentNode, j ,Destination};
+                        return route;
+                    }
+                    else{
+                        return route = {CurrentNode,Destination};
+                    }
                 }
                 else{
                     for(int k=0; k<7;k++){
-                        cout << "For loop 2" << endl;			//debugging line
                         if (matrix[j][k] != 0 && k != CurrentNode){
-                            cout << "If loop 2"<< endl;			//debugging line
                             if (matrix[k][Destination] > 0){
-                                cout << "Path is" << CurrentNode << " -> " << j << " -> " << k << " -> " << Destination << endl;
-                                return 1;
+                                if ((k != 5 && k != 6) && (j !=5 && j !=6)){
+                                    route = {CurrentNode, j, k ,Destination};
+                                    return route;
+                                }
+                                else if((j !=5 && j !=6) && (k == 5 && k==6)){
+                                    route = {CurrentNode,j, Destination };
+                                }
+                                else if((k !=5 && k !=6) && (j == 5 || j == 6)){
+                                    route = {CurrentNode, k ,Destination};
+                                    return route;
+                                }
                             }
-                            else if (matrix[k][Destination] < 0){
-                                cout << "Path is" << CurrentNode << " -> " << j << " -> " << k << " -> " << Destination << endl;
-                                return -1;
+                        }
+                   }
+                }
+            }
+        }
+    }
+        if((CurrentNode == 1 && Destination == 0)||(CurrentNode == 1 && Destination == 2)){
+           for(int j=0; j<7;j++){
+              if (matrix[CurrentNode][j] != 0){
+                if (matrix[j][Destination] != 0){
+                    if (j != 5 && j != 6){
+                        route = {CurrentNode, j ,Destination};
+                        return route;
+                    }
+                    else{
+                        return route = {CurrentNode,Destination};
+                    }
+                }
+                else{
+                    for(int k=0; k<7;k++){
+                        if (matrix[j][k] != 0 && k != CurrentNode){
+                            if (matrix[k][Destination] > 0){
+                                if ((k != 5 && k != 6) && (j !=5 && j !=6)){
+                                    route = {CurrentNode, j, k ,Destination};
+                                    return route;
+                                }
+                                else if((j !=5 && j !=6) && (k == 5 && k==6)){
+                                    route = {CurrentNode,j, Destination };
+                                }
+                                else if((k !=5 && k !=6) && (j == 5 || j == 6)){
+                                    route = {CurrentNode, k ,Destination};
+                                    return route;
+                                }
+                            }
+                        }
+                   }
+                }
+            }
+        }
+    }
+        for(int j=7; j>0;j--){
+           if (matrix[CurrentNode][j] != 0){
+                if (matrix[j][Destination] > 0){
+                    if (j != 5 && j != 6){
+                        route = {CurrentNode, j ,Destination};
+                        return route;
+                    }
+                    else{
+                        return route = {CurrentNode,Destination};
+                    }
+                }
+                else{
+                    for(int k=0; k<7;k++){
+                        if (matrix[j][k] != 0 && k != CurrentNode){
+                            if (matrix[k][Destination] > 0){
+                                if ((k != 5 && k != 6) && (j !=5 && j!=6)){
+                                    route = {CurrentNode, j ,k ,Destination};
+                                    return route;
+                                }
+                                else if((j !=5 && j !=6) && (k == 5 && k==6)){
+                                    route = {CurrentNode,j, Destination };
+                                }
+                                else if((k !=5 && k !=6) && (j == 5 || j == 6)){
+                                    route = {CurrentNode, k ,Destination};
+                                    return route;
+                                }
                             }
                         }
                    }
@@ -50,18 +127,15 @@ int path(const vector<vector<int>>& matrix, int CurrentNode, int Destination){
     }
 }
 
-
-
-int main()
-{
-    vector<vector<int>> matrix = {{ 0, 0, 0, 0, 3, -2, 0 },
-                                  { 0, 0, 0, 0, 0, 1, -1 },
-                                  { 0, 0, 0, 3, 0, -2, 0 },
-                                  { 0, 0, -6, 0, 0, 0, 5 },
-                                  { 6, 0, 0, 0, 0, 0, -5 },
-                                  { -2, 1, 2, 0, 0, 0, 0 },
-                                  { 0, 1, 0, -5, 5, 0, 0 }};
-
-    path(matrix,0,3);
+void loop(){
+  std::vector<int> PrintPath = path(matrix,CurrentNode,Destination);
+  for (int i = 0; i < PrintPath.size(); i++) {
+    Serial.println(PrintPath[i]);
+    if (i < PrintPath.size() - 1)  Serial.println(" -> ");  // Print arrows between nodes
+  }
+  Serial.println("\n");
 }
+
+
+
 
